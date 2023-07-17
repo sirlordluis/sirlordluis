79941b8e3fc5ccddd86e9b54fbab45af202e071a
sirlordluis <lordbanana1@gmail.com>
2023-07-16 23:11:57 -0600

    gradiant App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,57 @@
-
-import './css/App.css'
+import { useEffect, useRef } from 'react';
+import './styles/App.css';
 
 function App() {
+  const appRef = useRef(null);
+/*COLOCAR EL GRADIENTE AL INICIO*/
+  useEffect(() => {
+    const setGradiantBackground = () => {
+      const winWidth = window.innerWidth;
+      const winHeight = window.innerHeight;
+
+      const mouseX = Math.round((winWidth / 2) * 100);
+      const mouseY = Math.round((winHeight / 2) * 100);
+
+      if (appRef.current) {
+        appRef.current.style.setProperty('--mouse-x', mouseX.toString() + '%');
+        appRef.current.style.setProperty('--mouse-y', mouseY.toString() + '%');
+      }
+    };
+/*USAR GRADIENTE AL MOVER EL MOUSE*/
+    setGradiantBackground();
+
+    const moveGradiant = (event) => {
+      const winWidth = window.innerWidth;
+      const winHeight = window.innerHeight;
+
+      const mouseX = Math.round((event.pageX / winWidth) * 100);
+      const mouseY = Math.round((event.pageY / winHeight) * 100);
+
+      if (appRef.current) {
+        appRef.current.style.setProperty('--mouse-x', mouseX.toString() + '%');
+        appRef.current.style.setProperty('--mouse-y', mouseY.toString() + '%');
+      }
+    };
+    document.addEventListener('mousemove', moveGradiant);
+    return () => {
+      document.removeEventListener('mousemove', moveGradiant);
+    };
+  });
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      
+      <div className="app" id="app" ref={appRef} data-scroll-container>
+        <div className="blink justify-center flex align-center">
+          
+        </div>
+      </div>
+      
+      <h1 className='custom-font'>Welcome!</h1>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
